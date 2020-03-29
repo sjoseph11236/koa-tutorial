cc33589117aedc2e24a1223cbaa48cd513ca663c
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-03-28 20:52:02 -0400

    feat: created test route

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const Koa = require('koa');
+const KoaRouter = require('koa-router');
+const json = require('koa-json');
 
+//  Intialize app.
 const app = new Koa();
+const router = new KoaRouter();
+
 const PORT = 3000; 
 
-app.use(async ctx => ctx.body = 'Hello World');
+
+// JSON Prettier Middleware
+app.use(json());
+
+// Simple Middlware Examples
+// app.use(async ctx => ctx.body = 'Hello World!');
+// app.use(async ctx => ctx.body = {"msg": "Hello World"});
+
+router.get('/test', ctx => ( ctx.body = "Hello Test"));
+
+// Router Middleware
+app.use(router.routes()).use(router.allowedMethods());
 
 app.listen(PORT, () => console.log(`Server is listening on port: ${PORT}`));
