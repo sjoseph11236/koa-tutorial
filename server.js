e4bca85e5ea80a222c06d84d49da09876510926e
Sayeed Joseph <sayeedjoseph@gmail.com>
2020-03-28 21:34:48 -0400

    feat: add navbar

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 const Koa = require('koa');
 const KoaRouter = require('koa-router');
 const json = require('koa-json');
+const path = require('path');
+const render = require('koa-ejs');
 
 //  Intialize app.
 const app = new Koa();
 const router = new KoaRouter();
 
 const PORT = 3000; 
-
+// Replace with DB
 
 // JSON Prettier Middleware
 app.use(json());
@@ -15,6 +17,22 @@ app.use(json());
 // Simple Middlware Examples
 // app.use(async ctx => ctx.body = 'Hello World!');
 // app.use(async ctx => ctx.body = {"msg": "Hello World"});
+render(app, {
+  root: path.join(__dirname, 'views'),
+  layout: 'layout',
+  viewExt:'html', 
+  cache: false, 
+  debug: false
+});
+
+router.get('/', async ctx => {
+  await ctx.render('index', {
+    title: "Things I Love:"
+  });
+});
+
+// Index
+router.get('/', async ctx => await ctx.render('index'));
 
 router.get('/test', ctx => ( ctx.body = "Hello Test"));
 
